014a11917dbe9c5ef4fbb4e7f0b7eb1df1eac04b
satoshi_kimura <>
2020-10-20 16:44:35 +0900

    [save]フラグメント遷移のみの状態

--- app/src/main/java/com/example/bluetoothsample/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothsample/MainActivity.kt
@@ -1,26 +1,10 @@
 package com.example.bluetoothsample
 
 import BluetoothConnect
-import android.bluetooth.BluetoothDevice
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    //  IntentFilterの設定。
-    //  優先度を最大の999 (SYSTEM_HIGH_PRIORITY - 1) にしています。
-    private val pairingRequestIntentFilter: IntentFilter by lazy {
-        IntentFilter(BluetoothDevice.ACTION_PAIRING_REQUEST).also {
-            it.priority = IntentFilter.SYSTEM_HIGH_PRIORITY - 1
-        }
-    }
-
-    //  BroadcastReceiver
-    private var pairingBroadcastReceiver: PairingBroadcastReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,54 +19,5 @@ class MainActivity : AppCompatActivity() {
         // commit()で反映を行う
         fragmentTransaction.commit()
 
-
-
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        //  ここでBroadcastReceiverの登録を行っています。
-        this.pairingBroadcastReceiver = PairingBroadcastReceiver()
-        this.registerReceiver(this.pairingBroadcastReceiver, this.pairingRequestIntentFilter)
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        //  ここでBroadcastReceiverの登録解除を行っています。
-        this.unregisterReceiver(this.pairingBroadcastReceiver)
-        this.pairingBroadcastReceiver = null
-    }
-
-    //  ペアリング //  Android標準の場合
-    //  (this.adapter: BluetoothAdapter)
-    private fun pair(address: String, pin: ByteArray) {
-        this.pairingBroadcastReceiver!!.pin = pin
-        val bluetoothDevice = this.adapter.getRemoteDevice(address)
-        bluetoothDevice.createBond()
-    }
-}
-
-class PairingBroadcastReceiver : BroadcastReceiver() {
-
-    //  PINコードの受け渡し口
-    //  (サンプルコードなので雑でも気にしないでね♡)
-    var pin: ByteArray? = null
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent == null) return
-
-        if (intent.action == BluetoothDevice.ACTION_PAIRING_REQUEST) {
-            val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)!!
-            val pin = this.pin!!
-
-            device.setPin(pin)
-
-            //  他のBroadcastReceiverにメッセージが飛んでいかなないようにする。
-            //  これを呼ばないとシステムのペアリング要求通知が出てきてしまう。
-            this.abortBroadcast()
-        }
     }
 }
