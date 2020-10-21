808600a3f7056900c99825a6edcd3c1253c182da
satoshi_kimura <>
2020-10-21 15:38:26 +0900

    [conprete]接続の試行まで一旦完成

--- app/src/main/java/com/example/bluetoothsample/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothsample/MainActivity.kt
@@ -1,38 +1,17 @@
 package com.example.bluetoothsample
 
-import BluetoothConnect
-import android.bluetooth.*
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-//BLE
-private var mBluetoothAdapter: BluetoothAdapter? = null
-private val mBleGatt: BluetoothGatt? = null
-private val mBluetoothGattCharacteristic: BluetoothGattCharacteristic? = null
-
-//ACTION_FOUNDのBroadcastReceiverを作成
-val receiver = object : BroadcastReceiver() {
-
-    override fun onReceive(context: Context, intent: Intent) {
-        val action: String? = intent.action
-        when(action) {
-            BluetoothDevice.ACTION_FOUND -> {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                val deviceName = device.name
-                val deviceHardwareAddress = device.address // MAC address
-
-            }
-        }
-    }
-}
+var mBluetoothLeScanner: BluetoothLeScanner? = null
+var mScanCallback: ScanCallback? = null
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,69 +19,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Fragmentを生成するためのFragmentTransactionの準備をして
-        val firstFragment = BluetoothConnect()
-        // FirstFragment.javaのonCreateView()がスタート
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        // add()でフラグメントの追加、 add()の第一引数には、Fragmentの表示を行う場所のid、第二引数は表示したいFragmentを指定
-        fragmentTransaction.add(R.id.fragment_container, firstFragment)
-        // commit()で反映を行う
-        fragmentTransaction.commit()
-
-
-
         button.setOnClickListener{
-            var bluetoothHeadset: BluetoothHeadset? = null
-
-// Get the default adapter
             val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-            // BluetoothAdapter を取得
+
+            // Bluetoothサポートしているかのチェック
             if (bluetoothAdapter == null) {
-                // Device doesn't support Bluetooth
-            }
-            // Bluetooth を有効にする
-            if (bluetoothAdapter?.isEnabled == false) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, 0)
+                Toast.makeText(this, "Bluetooth未サポート", Toast.LENGTH_SHORT)
+                finish()
             }
 
-            val profileListener = object : BluetoothProfile.ServiceListener {
+            mBluetoothLeScanner = bluetoothAdapter?.bluetoothLeScanner
+            // kotlin プロパティ優先なのでメソッド使うな
 
-                override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
-                    if (profile == BluetoothProfile.HEADSET) {
-                        bluetoothHeadset = proxy as BluetoothHeadset
-                    }
-                }
+            mScanCallback = initCallbacks()
+            println(mScanCallback.toString())
+            println("mScanCallback")
 
-                override fun onServiceDisconnected(profile: Int) {
-                    if (profile == BluetoothProfile.HEADSET) {
-                        bluetoothHeadset = null
-                    }
-                }
-            }
+            // スキャンの開始
+            mBluetoothLeScanner?.startScan(mScanCallback)
 
-// プロキシへの接続を確立
-            bluetoothAdapter?.getProfileProxy(this, profileListener, BluetoothProfile.HEADSET)
+            // スキャンの停止
+            // mBluetoothLeScanner?.stopScan(mScanCallback)
 
-// ... call functions on bluetoothHeadset
-
-
-
-
-            // Register for broadcasts when a device is discovered.
-            val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-            registerReceiver(receiver, filter)
-
-            // Close proxy connection after use.
-            bluetoothAdapter?.closeProfileProxy(BluetoothProfile.HEADSET, bluetoothHeadset)
         }
+    }
 
+    private fun initCallbacks(): ScanCallback? {
+        return object : ScanCallback() {
+            override fun onScanResult(
+                callbackType: Int,
+                result: ScanResult
+            ) {
+                super.onScanResult(callbackType, result)
 
-
-        fun onDestroy() {
-            super.onDestroy()
-            // Don't forget to unregister the ACTION_FOUND receiver.
-            unregisterReceiver(receiver)
+                // デバイスが見つかった！
+                if (result != null && result.device != null) {
+                    // リストに追加などなどの処理をおこなう
+                    //addDevice(result.getDevice(), result.getRssi());
+                    println(result.getDevice())
+                    println(result.getRssi())
+                }
+                return
+            }
         }
     }
 }
